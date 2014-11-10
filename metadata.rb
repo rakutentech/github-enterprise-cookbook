7298b04f0a6370e0da550fecf3b3c90947b17d6b
Graham Weldon <graham.weldon@mail.rakuten.com>
2014-11-10 14:45:19 +0900

    Added platform support to metadata, bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,14 @@ maintainer_email 'ops@rakops.com'
 license          'Apache 2.0'
 description      'Configures utilities for Github Enterprise'
 long_description 'Configures utilities for Github Enterprise'
-version          '0.1.1'
+version          '0.1.2'
+
+supports         'redhat'
+supports         'centos'
+supports         'fedora'
+supports         'debian'
+supports         'ubuntu'
 
 depends          'git'
 depends          'cron'
+
