e8c9ca1ea6b40faa4aecabe25685ab924ddf3050
Graham Weldon <graham.weldon@rakuten.com>
2016-02-07 20:01:02 -0500

    Refactor property names, and include config

--- test/fixtures/cookbooks/github-enterprise_test/recipes/backup.rb
+++ test/fixtures/cookbooks/github-enterprise_test/recipes/backup.rb
@@ -1,4 +1,7 @@
-ghe_backup_utils '/opt/github/backup-utils' do
+ghe_dir = '/opt/github/backup-utils'
+ghe_config = ::File.join(ghe_dir, 'backup.config')
+
+ghe_backup_utils ghe_dir do
   # hostname 'github.example.com'
   # dir '/opt/github-backup-utils'
   # data_dir '/opt/github-backup-data'
@@ -10,6 +13,13 @@ ghe_backup_utils '/opt/github/backup-utils' do
   action :create
 end
 
+ghe_backup_config ghe_config do
+  hostname 'github.mycompany.com'
+  action :create
+end
+
 ghe_backup_cron 'default' do
+  dir ghe_dir
+  config_file ghe_config
   action :create
 end
